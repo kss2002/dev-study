4d78216b585e63a9bbfa8168ab9cb3760a4795d0
seong su kim <know.warehouse02@gmail.com>
2025-05-09 18:36:13 +0900

    header 구조 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Logo</h1>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
