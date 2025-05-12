aad079cd24421a4bc50b061d12b66350109ef72a
seong su kim <know.warehouse02@gmail.com>
2025-05-12 19:37:56 +0900

    main add

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import '../assets/styles/Main.css';
+
+const Main = () => {
+  return (
+    <>
+      <div>
+        <h1 className="main-title">몰입, 집중, 성장</h1>
+      </div>
+    </>
+  );
+};
+
+export default Main;
